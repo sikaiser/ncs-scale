c900c70d9da3906477d428d58e5889b4287f64ad
sikaiser <simon@emperor.ch>
2025-11-16 16:44:51 +0100

    keep main alive

--- src/main.c
+++ src/main.c
@@ -2,8 +2,18 @@
 
 #include "channels.h"
 
+#define LOG_MODULE_NAME main
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
+
 int main(void)
 {
         zbus_print_channels_and_observers();
+
+        while (1) {
+                LOG_INF("Main thread heartbeat");
+                k_msleep(2000); 
+        }
+
 	return 0;
 }
